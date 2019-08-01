57a764d63aca718302bfbea0c1316ea23f11e394
Ethan Tran <ethan.tran@7-11.com>
2019-07-31 19:04:50 -0500

    Update removeDups.js

--- removeDups.js
+++ removeDups.js
@@ -1,18 +1,22 @@
+function isDup(a, target) {
+  let curr = a;
+  while (curr !== target) {
+    if (curr.value === target.value) {
+      return true;
+    }
+    curr = curr.next;
+  }
+  return false;
+}
+
 function removeDups(a) {
   let curr = a;
   while (curr) {
-    if (curr.next) {
-      let curr2 = curr.next;
-      while (curr2) {
-        console.log(curr.value);
-        console.log(curr2.value);
-        if (curr.value === curr2.value) {
-          curr.next = curr.next.next;
-        }
-        curr2 = curr2.next;
-      }
+    if (curr.next && isDup(a, curr.next)) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
     }
-    curr = curr.next;
   }
 }
 
@@ -40,12 +44,14 @@ let n2 = Object.create(Node).init("2");
 let n2_2 = Object.create(Node).init("2");
 let n3 = Object.create(Node).init("3");
 let n3_2 = Object.create(Node).init("3");
+let n3_3 = Object.create(Node).init("3");
 
 n1.next = n2;
 n2.next = n1_2;
 n1_2.next = n3_2;
 n3_2.next = n2_2;
 n2_2.next = n3;
+n3.next = n3_3;
 
 console.log(n1.toString());
 removeDups(n1);
